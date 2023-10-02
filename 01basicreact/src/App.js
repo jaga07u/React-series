7587c02ca0833561ed3a5464113a529a67f511b2
jaga07u <pradhan02jagadish@gmail.com>
2023-10-02 19:48:46 +0530

    basic react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from './chai'
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+  </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+function App() {
+  return   (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
